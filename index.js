190dd2c14664ec2ddb5361b1d7a6cceee51c452a
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-03-19 17:53:59 +0300

    completed project

--- index.js
+++ index.js
@@ -1,82 +1,41 @@
-function Total(){
-    var sub1 = parseInt(document.getElementById("python").value);
-    var sub2 = parseInt(document.getElementById("javascript").value);
-    var sub3 = parseInt(document.getElementById("html").value);
-    var sub4 = parseInt(document.getElementById("css").value);
-   
-    
+// total marks//
+function calculateGrade() {
+  // Get the input values and convert them to numbers
+  var name = document.getElementById("name").value;
+  var mark1 = Number(document.getElementById("python").value);
+  var mark2 = Number(document.getElementById("javascript").value);
+  var mark3 = Number(document.getElementById("html").value);
+   // validation 
+if(name==null || name==""){
+  alert("please enter name");
+  return;
+}
+
+  if (mark1 < 0 || mark1 > 100 || mark2 < 0 || mark2 > 100 || mark3 < 0 || mark3 > 100) {
+    alert("Please enter marks between 0-100");
+  	return;
+			}
+
+  // Calculate the total and average marks
+  var total = mark1 + mark2 + mark3;
+  var average = total / 3;
   
-    if(sub1>100 || sub2>100 || sub3>100 || sub4>100 || sub5>100 )
-    {
-      alert("Please Enter Marks in range of 100");
-    }
-    else {
-      var total= sub1 + sub2 + sub3 + sub4 ;
-      document.getElementById("total").innerHTML = "python Marks :"+sub1+"<br> javascript Marks: "+sub2+"<br> html Marks: "+sub3+"<br> Css Marks: "+sub4+"<br> Total Marks: "+total;  
-    }
+  // Determine the grade based on the average mark
+  var grade;
+  if (average > 79) {
+    grade = "A";
+  } else if (average >= 60 && average <= 79) {
+    grade = "B";
+  } else if (average >= 50 && average <= 59) {
+    grade = "C";
+  } else if (average >= 40 && average <= 49) {
+    grade = "D";
+  } else {
+    grade = "E";
   }
   
-  function Average(){
-    var sub1 = parseInt(document.getElementById("Python").value);
-    var sub2 = parseInt(document.getElementById("javascript").value);
-    var sub3 = parseInt(document.getElementById("html").value);
-    var sub4 = parseInt(document.getElementById("css").value);
-    
-      
-    if(sub1>100 || sub2>100 || sub3>100 || sub4>100 || sub5>100 )
-    {
-      alert("Please Enter Marks in range of 100");
-    }
-    else {
-      var total= sub1 + sub2 + sub3 + sub4 + sub5;
-      var avg=total/5;
-      document.getElementById("avg").innerHTML="Your Average marks are: "+avg;
-    }
-  }
-    
-    
-  function Grade(){
-     var sub1 = parseInt(document.getElementById("Python").value);
-    var sub2 = parseInt(document.getElementById("javascript").value);
-    var sub3 = parseInt(document.getElementById("html").value);
-    var sub4 = parseInt(document.getElementById("css").value);
-   
-     if(sub1>100 || sub2>100 || sub3>100 || sub4>100 || sub5>100 )
-    {
-      alert("Please Enter Marks in range of 100");
-    }else {
-       var total= sub1 + sub2 + sub3 + sub4 + sub5;
-       var avg=total/5;
-       
-       if(avg>=80 && avg<=100)
-       {
-         document.getElementById("grade").innerHTML="You Got A+ Grade";
-       }
-       else if(avg>=75 && avg<=80)
-       {
-         document.getElementById("grade").innerHTML="You Got A+ Grade";
-       }
-       else if(avg>=70 && avg<=75)
-       {
-         document.getElementById("grade").innerHTML="You Got A Grade";
-       }
-       else if(avg>=65 && avg<=70)
-       {
-         document.getElementById("grade").innerHTML="You Got B Grade";
-       }
-       else if(avg>=50 && avg<=60)
-       {
-         document.getElementById("grade").innerHTML="You Got C Grade";
-       }
-       else if(avg>=40 && avg<=50)
-       {
-         document.getElementById("grade").innerHTML="You Got C Grade";
-       }
-       
-       else {
-         document.getElementById("grade").innerHTML="You Got F Grade";
-       }
-       
-      
-    }
-  }  
+  // Output the grade
+  document.getElementById("gradeOutput").innerHTML = "Student grade is: " + grade;
+  document.getElementById("totaloutput").innerHTML = "student total is:" + total;
+  document.getElementById("avgoutput").innerHTML = "Student average is: " + average;
+}
